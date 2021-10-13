4d2f05c6b515aec60d6f88635a8a9dba4692e391
troy213 <tritera.erlangga@gmail.com>
2021-10-13 08:33:10 +0700

    add project item

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import projectBlob from '../assets/projects.svg'
 import marketImg from '../assets/market.png'
+import ongkirImg from '../assets/ongkir.png'
 
 const Projects = () => {
   return (
@@ -41,6 +42,27 @@ const Projects = () => {
                   A fully functioning RESTful e-commerce website project with
                   ReactJS, NodeJS, MySQL, JWT Authentication, and BCrypt hashing
                   algorithm for storing passwords.
+                </p>
+              </div>
+            </a>
+            <a
+              href='https://app.triteraerlangga.com'
+              className='projects-card-item'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <div className='projects-card-img-wrapper'>
+                <img
+                  src={ongkirImg}
+                  alt='ongkir'
+                  className='projects-card-img'
+                />
+              </div>
+              <div className='projects-card-desc'>
+                <h3>3rd Party API Web App</h3>
+                <p>
+                  A simple website using external API to check delivery cost
+                  estimation from some courier services.
                 </p>
               </div>
             </a>
